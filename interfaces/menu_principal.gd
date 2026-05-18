3dfbfe13c14fd094522114e26cb267a467a81899
Pedro Stelzer <stelzerpedro2@gmail.com>
2026-05-18 14:04:09 -0300

    feat: adicionei as telas do menu principal e creditos

---
+++ interfaces/menu_principal.gd
@@ -0,0 +1,26 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+@warning_ignore("unused_parameter")
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_play_button_pressed() -> void:
+	print("Iniciar Jogo")
+	get_tree().change_scene_to_file("res://main.tscn")
+
+
+func _on_exit_button_pressed() -> void:
+	print("Saiu do jogo")
+	get_tree().quit()
+
+
+func _on_creditos_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://interfaces/creditos.tscn")
