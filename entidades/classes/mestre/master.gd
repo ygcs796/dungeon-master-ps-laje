5fdd5d8d45b10ae52cbb7f380b0ac02e316229f4
Pedro Stelzer <stelzerpedro2@gmail.com>
2026-05-22 16:42:00 -0300

    feat: adicionei a promeira cena do mural de quests e a classe do mestre do jogo

---
+++ entidades/classes/mestre/master.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+@export var pontos_disponiveis : int
+@export var nome : String
+@export var narrativa : int
+@export var persuasao : int
+@export var criatividade : int
+@export var sorte : int
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- interfaces/menu_principal.gd
+++ interfaces/menu_principal.gd
@@ -14,7 +14,7 @@ func _process(delta: float) -> void:
 
 func _on_play_button_pressed() -> void:
 	print("Iniciar Jogo")
-	get_tree().change_scene_to_file("res://main.tscn")
+	get_tree().change_scene_to_file("res://interfaces/cena_1_mural_quests.tscn")
 
 
 func _on_exit_button_pressed() -> void:
