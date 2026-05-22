531797b4bb8787689e3bb049cec84ef216ef142b
Pedro Stelzer <stelzerpedro2@gmail.com>
2026-05-22 18:26:22 -0300

    feat: inicio do progress bar

--- interfaces/cena_2_mapa.gd
+++ interfaces/cena_2_mapa.gd
@@ -9,3 +9,14 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func iniciar_barra_de_progresso():
+	
+	var barra = $ProgressBar
+	barra.value = 0
+	
+	for i in range(1, 8):
+		
+		await get_tree().create_timer(1.0)
+		barra.value = i
+		
